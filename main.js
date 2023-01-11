0e19c1775bd1a27fc263d1c0c51e7545e394ccf2
Farmata-sidibe <69140880+Farmata-sidibe@users.noreply.github.com>
2023-01-11 15:15:37 +0100

    integration du menu burger

--- main.js
+++ main.js
@@ -2,25 +2,28 @@ const btnMenuVisible = document.querySelector('#menu_visible');
 const btnMenuHidden = document.querySelector('#menu_hidden');
 const navBurger = document.querySelector('.nav_burger');
 
-// window.onload = function(){
-//     if(window.matchMedia("(max-width: 768px)").matches){
-//         btnMenuVisible.style.display = 'block';
-//     }
-// }
+// RESPONSIVE FONCTION
+window.addEventListener('resize', () => {
+    if (window.innerWidth >= 768) {
+        btnMenuVisible.style.display = 'none';
 
-
-
+    } else {
+        btnMenuVisible.style.display = 'block';
+    }
+})
+// Responsive active burger
 btnMenuVisible.addEventListener('click', function(){
     console.log('active')
-    if((btnMenuVisible.style.display = 'block') && (window.matchMedia("(max-width: 768px)").matches)){
+    if((btnMenuVisible.style.display = 'block') && (window.matchMedia("(max-width: 992px)").matches)){
         btnMenuHidden.style.display = 'block';
         btnMenuVisible.style.display = 'none';
         navBurger.style.display = 'flex';
     }
 })
+// Responsive close burger
 btnMenuHidden.addEventListener('click', function(){
     console.log('close')
-    if((btnMenuHidden.style.display = 'block') && (window.matchMedia("(max-width: 768px)").matches)){
+    if((btnMenuHidden.style.display = 'block') && (window.matchMedia("(max-width: 992px)").matches)){
         btnMenuHidden.style.display = 'none';
         btnMenuVisible.style.display = 'block';
         navBurger.style.display = 'none';
